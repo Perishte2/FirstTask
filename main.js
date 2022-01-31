352c5aa4888664e6e4ed2c07b348bb02c315885c
Perishte <perishte010203@gmail.com>
2022-02-01 01:13:00 +0600

    sixth task about current time

--- main.js
+++ main.js
@@ -1,4 +1,9 @@
 let p = document.getElementById('p');
-
 let date = new Date();
 p.textContent ="Current time : " +  date.getHours()  + " : " + date.getMinutes() + " : " + date.getSeconds();
+
+
+
+
+
+
